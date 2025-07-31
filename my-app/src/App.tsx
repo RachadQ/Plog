6a62d1d52780b6d53964466bbaa0d8906479ff2b
Rachad <36934217+RachadQ@users.noreply.github.com>
2025-07-31 19:47:59 -0400

    Added google ads.txt

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import UserProfile from './components/userProfile';
 import { Route, Routes  } from 'react-router-dom';
